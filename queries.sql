17817885a67b61cd4819c2a10af362c01265db94
Bryan Kwong <bfkwong@calpoly.edu>
2019-10-20 10:53:25 -0700

    Finalized it

--- queries.sql
+++ queries.sql
@@ -1,8 +1,16 @@
+-------------------
+------- # 1 -------
+-------------------
+
 SELECT last, first, Player.id, count(Player.id) as goals
 FROM Player INNER JOIN Event ON Player.id = Event.player_id
     WHERE type="Goals scored"
     GROUP BY Player.id
     HAVING goals >= 3;
+
+-------------------
+------- # 2 -------
+-------------------
 
 SELECT DISTINCT shortName, name
 FROM (
@@ -13,8 +21,68 @@ FROM (
         HAVING goals >= 3
 ) as Player RIGHT JOIN Team ON Player.team_id = Team.id;
 
-SELECT *
+-------------------
+------- # 3 -------
+-------------------
+
+(SELECT Team.id, Team.name
 FROM (
     SELECT * FROM Game
-        WHERE score_home > 3 OR score_away > 3
-) as Teams LEFT JOIN Team ON Teams.home_team_id = Team.id;
+    WHERE score_home > 3
+) as Teams LEFT JOIN Team ON Teams.home_team_id = Team.id)
+UNION
+(SELECT Team.id, Team.name
+FROM (
+    SELECT * FROM Game
+    WHERE score_away > 3
+) as Teams LEFT JOIN Team ON Teams.away_team_id = Team.id);
+
+-------------------
+------- # 4 -------
+-------------------
+
+SELECT Winner FROM
+    (SELECT Winner, COUNT(Winner) as Wins FROM (
+        SELECT id,
+            CASE
+                WHEN score_home > score_away THEN score_home
+                WHEN score_home < score_away THEN score_away
+                ELSE "tie"
+            END AS Winner
+        FROM Game
+    ) as Results GROUP BY Winner ORDER BY Wins DESC)
+as Wins WHERE Winner <> "tie" GROUP BY Winner LIMIT 1;
+
+-------------------
+------- # 5 -------
+-------------------
+
+SELECT id, away_team_id as opponent FROM
+(SELECT Winner FROM
+    (SELECT Winner, COUNT(Winner) as Wins FROM (
+        SELECT id,
+            CASE
+                WHEN score_home > score_away THEN score_home
+                WHEN score_home < score_away THEN score_away
+                ELSE "tie"
+            END AS Winner
+        FROM Game
+    ) as Results GROUP BY Winner ORDER BY Wins DESC)
+as Wins WHERE Winner <> "tie" GROUP BY Winner LIMIT 1) as Winner
+JOIN
+Game ON Winner.Winner = Game.home_team_id WHERE score_home < score_away
+UNION
+SELECT id, home_team_id as opponent  FROM
+(SELECT Winner FROM
+    (SELECT Winner, COUNT(Winner) as Wins FROM (
+        SELECT id,
+            CASE
+                WHEN score_home > score_away THEN score_home
+                WHEN score_home < score_away THEN score_away
+                ELSE "tie"
+            END AS Winner
+        FROM Game
+    ) as Results GROUP BY Winner ORDER BY Wins DESC)
+as Wins WHERE Winner <> "tie" GROUP BY Winner LIMIT 1) as Winner
+JOIN
+Game ON Winner.Winner = Game.away_team_id WHERE score_home > score_away;
